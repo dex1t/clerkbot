f4d4deebc1b294f007504b5607f488753ba4bc93
dex1t <dex1t@degoo.org>
2017-05-15 01:40:38 +0900

    Add empty message

--- app/skills/stats.js
+++ app/skills/stats.js
@@ -23,17 +23,20 @@ class Stats {
   run() {
     this.controller.hears('stats', ['direct_message', 'direct_mention', 'mention'], (bot, msg) => {
       const fetchUnassignedCount = this.client.counts.conversationCounts()
-          .then(r => r.body.conversation.unassigned);
+          .then(r => r.body.conversation);
       const fetchAdminAssignedCount = this.client.counts.conversationAdminCounts()
           .then(r => r.body.conversation.admin.filter(v => v.open > 0));
 
       Promise.all([fetchUnassignedCount, fetchAdminAssignedCount]).then((r) => {
-        // console.log(this.buildAttachment(r[0], r[1]));
-        bot.say({
-          channel: msg.channel,
-          text: 'This is the current open conversation :mailbox_with_mail:',
-          attachments: [this.buildAttachment(r[0], r[1])],
-        });
+        if(r[0].open === 0) {
+          bot.reply(msg, 'All conversations have been replied :clap:');
+        } else {
+          bot.say({
+            channel: msg.channel,
+            text: 'This is the current open conversation :mailbox_with_mail:',
+            attachments: [this.buildAttachment(r[0].unassigned, r[1])],
+          });
+        }
       });
     });
   }
